93af04e14dd54b5261e14e9af18d4fd57247cc54
Magnarius Cosmo <pvdogh@protonmail.com>
2026-06-04 19:35:56 +0200

    fix to task ring

--- mt/task_ring.hpp
+++ mt/task_ring.hpp
@@ -1,90 +1,91 @@
 #pragma once
 
-#include <atomic>
 #include <array>
+#include <atomic>
 #include <chrono>
 #include <semaphore>
 
 namespace mt
 {
-	template<typename Type, std::size_t Size>
-	class TaskRing
-	{
-	private:
-		static_assert((Size & (Size - 1)) == 0, "TaskRing size must be power of 2");
+    namespace impl
+    {
+        inline void yield() noexcept
+        {
+#if defined(_MSC_VER)
+            _mm_pause();
+#elif defined(__GNUG__)
+            __builtin_ia32_pause();
+#else
+            std::this_thread::yield();
+#endif
+        }
+    } // namespace impl
 
-		static constexpr auto _mask = Size - 1;
+    template<typename Type, std::size_t Size> class TaskRing
+    {
+    private:
+        static_assert((Size & (Size - 1)) == 0, "TaskRing size must be power of 2");
 
-		// _tail stores the pointer to the current task that is next in line for execution
-		// _head stores the pointer of the next free task slot
-		// _head_check makes sure that the consumer does not grab a task that was reserved but not written to
+        static constexpr auto _mask = Size - 1;
 
-        /* data     */ std::array<Type, Size>        _ring{};
-        /* consumer */ std::atomic<std::size_t>      _tail{ 0 };
-        /* producer */ std::atomic<std::size_t>      _head{ 0 };
-        /* producer */ std::atomic<std::size_t>      _head_check{ 0 };
-        /* notify   */ std::counting_semaphore<Size> _available{ 0 };
-
-		static inline void _yield() noexcept
-		{
-#           if defined(_MSC_VER)
-				_mm_pause();
-#           elif defined(__GNUG__)
-				__builtin_ia32_pause();
-#           else
-				std::this_thread::yield();
-#           endif
-		}
+        /* data     */ std::array<Type, Size> _ring{};
+        /* consumer */ std::atomic<std::size_t> _tail{0};
+        /* producer */ std::atomic<std::size_t> _head{0};
+        /* producer */ std::atomic<std::size_t> _head_check{0};
+        /* notify   */ std::counting_semaphore<Size> _available{0};
 
         bool _enqueue_impl(bool wait, Type& value) noexcept
-		{
-			std::size_t head = 0;
-			while (true)
-			{
-				head = _head.load();
-				if (head - _tail >= Size)
-				{
-					if (wait)
-					{
-						_yield();
-						continue;
-					}
-					return false;
-				}
-				if (_head.compare_exchange_weak(head, head + 1))
-					break;
-			}
-            _ring[head & _mask] = Type{ std::move(value) };
-			_head_check.fetch_add(1);
-			_head_check.notify_one();
-			_available.release();
-			return true;
-		}
-        std::optional<std::size_t> _dequeue_impl(Type& value) noexcept
         {
-            std::size_t tail = _tail.load();
-            if (const auto head = _head_check.load();
-                head <= tail)
-                return head;
+            std::size_t head = 0;
+            while (true)
+            {
+                head = _head.load(std::memory_order::relaxed);
+                if (head - _tail.load(std::memory_order::acquire) >= Size)
+                {
+                    if (wait)
+                    {
+                        impl::yield();
+                        continue;
+                    }
+                    return false;
+                }
+                if (_head.compare_exchange_weak(
+                        head, head + 1, std::memory_order::acq_rel, std::memory_order::relaxed
+                    ))
+                    break;
+            }
+
+            _ring[head & _mask] = Type{std::move(value)};
+            while (_head_check.load(std::memory_order::acquire) != head)
+                impl::yield();
+
+            _head_check.store(head + 1, std::memory_order::release);
+            _head_check.notify_one();
+            _available.release();
+
+            return true;
+        }
+        void _dequeue_ready(Type& value) noexcept
+        {
+            const auto tail = _tail.load(std::memory_order::relaxed);
+            while (_head_check.load(std::memory_order::acquire) <= tail)
+                _head_check.wait(tail, std::memory_order::acquire);
             value = std::move(_ring[tail & _mask]);
-            _tail.fetch_add(1);
-            return std::nullopt;
+            _tail.store(tail + 1, std::memory_order::release);
         }
     public:
         TaskRing() = default;
         TaskRing(const TaskRing&) = delete;
         TaskRing(TaskRing&& copy) noexcept {}
 
-        template<typename... Argv>
-        void enqueue(Argv&&... args) noexcept
+        template<typename... Argv> void enqueue(Argv&&... args) noexcept
         {
-            auto value = Type{ std::forward<Argv>(args)... };
+            auto value = Type{std::forward<Argv>(args)...};
             _enqueue_impl(true, value);
         }
-        template<typename... Argv>
-        bool try_enqueue(Argv&&... args) noexcept
+        template<typename... Argv> bool try_enqueue(Argv&&... args) noexcept
         {
-            auto value = Type{ std::forward<Argv>(args)... };
+            auto value = Type{std::forward<Argv>(args)...};
             return _enqueue_impl(false, value);
         }
 
@@ -99,50 +100,36 @@ namespace mt
 
         bool try_dequeue(Type& value) noexcept
         {
-            if (!_dequeue_impl(value).has_value())
-            {
-                _available.acquire();
-                return true;
-            }
-            return false;
+            if (!_available.try_acquire())
+                return false;
+            _dequeue_ready(value);
+            return true;
         }
         bool dequeue(Type& value) noexcept
         {
-            auto head = _dequeue_impl(value);
-            while (head.has_value())
-            {
-                _head_check.wait(head.value());
-                head = _dequeue_impl(value);
-            }
             _available.acquire();
+            _dequeue_ready(value);
             return true;
         }
         bool dequeue(Type& value, std::chrono::microseconds max) noexcept
         {
-            const auto beg = std::chrono::steady_clock::now();
-            do
-            {
-                if (std::chrono::steady_clock::now() - beg > max ||
-                    !_available.try_acquire_for(max))
-                {
-                    return false;
-                }
-            }
-            while (_dequeue_impl(value).has_value());
+            if (!_available.try_acquire_for(max))
+                return false;
+            _dequeue_ready(value);
             return true;
         }
 
         bool empty() const noexcept
         {
-            std::size_t tail = _tail.load();
-            if (const auto head = _head_check.load();
-                head <= tail)
-                return true;
-            return false;
+            const auto tail = _tail.load(std::memory_order::acquire);
+            const auto head = _head_check.load(std::memory_order::acquire);
+            return head <= tail;
         }
 
-		TaskRing& operator=(const TaskRing&) = delete;
-		TaskRing& operator=(TaskRing&&) noexcept { return *this; }
-	};
-}
-
+        TaskRing& operator=(const TaskRing&) = delete;
+        TaskRing& operator=(TaskRing&&) noexcept
+        {
+            return *this;
+        }
+    };
+} // namespace mt
